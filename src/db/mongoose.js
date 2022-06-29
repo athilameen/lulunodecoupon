f11fd1ee8d994aeb90931230c6c5eb59f7432c7a
vaisakh4de <vaisakh@dedoit.com>
2022-06-29 16:24:45 +0530

    Setup app for Heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,4 +4,8 @@ const bcrypt = require('bcryptjs')
 
 mongoose.connect('mongodb://127.0.0.1:27017/lulunod', {
     useNewUrlParser: true,
+}).then(() => {
+    console.log("DB Connected")
+}).catch(() => {
+    console.log("DB not Connected")
 })
