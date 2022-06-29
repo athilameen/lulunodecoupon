1446352bbd3643f7c79c14bf53efd8a49e7c2527
vaisakh4de <vaisakh@dedoit.com>
2022-06-29 17:22:42 +0530

    Setup app for Heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,11 @@
-require("dotenv").config()
+//require("dotenv").config()
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 
-mongoose.connect(process.env.DATABASE, {
+//process.env.DATABASE
+
+mongoose.connect('mongodb+srv://delunodeusr:YAeBE3zQuWbxrD4F@cluster0.ckaxj.mongodb.net/lulunod', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
