35c1a25ec4320749d96d9714e617f055462a7529
vaisakh4de <vaisakh@dedoit.com>
2022-06-29 14:50:37 +0530

    Setup app for Heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 
-mongoose.connect('mongodb://0.0.0.0:27017/blog', {
+mongoose.connect('mongodb://127.0.0.1:27017/lulunode', {
     useNewUrlParser: true,
 })
